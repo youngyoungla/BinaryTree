69c9e9426e75af7751a0dd5497ca8e092cbeede1
youngyoungla <634713275@qq.com>
2016-06-20 18:27:09 -0700

    modify

--- path_add.c
+++ path_add.c
@@ -2,9 +2,13 @@
 #include "binarytree.h"
 void test()
 {
-  int a[]={1,2,3,'#','#',4,'#','#',6,7};
-  BinaryTree b1(a,sizeof(a)/sizeof(a[0]));
-  b1.path_add(7);
+ // int a[]={1,2,3,'#','#',4,'#','#',6,7};
+  //BinaryTree b1(a,sizeof(a)/sizeof(a[0]));
+  //b1.path_add(7);
+  
+	int a[]={10,5,4,'#','#',7,'#','#',12};
+	BinaryTree b1(a,sizeof(a)/sizeof(a[0]));
+	b1.path_add(22);
 }
 int main()
 {
